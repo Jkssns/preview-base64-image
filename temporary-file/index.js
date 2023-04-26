7836a191bea375a2d10d7aef5a2276783500fc0a
zhangjun <zhangjun@acme-ai.com>
2023-04-26 13:28:17 +0800

    feat：增加上传缓存

--- temporary-file/index.js
+++ temporary-file/index.js
@@ -15,6 +15,20 @@ const client = new OSS({
 	bucket: 'temporary-files-b',
 });
 
+window.onload = () => {
+	const arr = JSON.parse(window.localStorage.getItem('uploadUrls') || "[]");
+	arr.forEach(item => {
+		if (Number(item.progress) < 100) {
+			client.abortMultipartUpload(item.fullname, item.uploadId);
+		} else {
+			uploadUrls.push(item);
+		}
+	});
+	if (uploadUrls.length) {
+		handleUploadOl();
+	}
+}
+
 // 获取取件码
 const getCode = (text = '请输入至少4位的，数字或字母组成的取件码。') => {
     const code = prompt(text);
@@ -48,17 +62,18 @@ fileDom.onchange = async e => {
 			}
 			uploadUrls.push(obj);
 			// 分片上传。
-			const res = client.multipartUpload(path + item.name, item, {
+			client.multipartUpload(path + item.name, item, {
 				progress: (p, cpt, res) => {
+					obj.uploadId = cpt.uploadId;
 					obj.progress = (p * 100).toFixed(2);
 					let dom = document.getElementById(obj.id);
 					if (dom) {
 						dom.querySelector('.file_item_percentage').innerText = obj.progress + '%';
 						if (p >= 1) {
-							handleUpdateloadOl();
+							handleUploadOl();
 						}
 					} else {
-						handleUpdateloadOl();
+						handleUploadOl();
 					}
 				},
 				mime: 'application/octet-stream',
@@ -70,7 +85,7 @@ fileDom.onchange = async e => {
 };
 
 // 更新上传列表
-const handleUpdateloadOl = () => {
+const handleUploadOl = () => {
     uploadWrapper.innerHTML = uploadUrls.map((item, index) => {
         return `<li class="file_item" id="${item.id}">
 					<span class="file_item_name">${item.name}</span>
@@ -78,6 +93,7 @@ const handleUpdateloadOl = () => {
 					<svg class="file_item_action" style="display: ${item.progress >= 100 ? 'inline' : 'none'}" data-fullname="${item.fullname}" data-index="${index}" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3578" width="24" height="24"><path d="M556.8 512L832 236.8c12.8-12.8 12.8-32 0-44.8-12.8-12.8-32-12.8-44.8 0L512 467.2l-275.2-277.333333c-12.8-12.8-32-12.8-44.8 0-12.8 12.8-12.8 32 0 44.8l275.2 277.333333-277.333333 275.2c-12.8 12.8-12.8 32 0 44.8 6.4 6.4 14.933333 8.533333 23.466666 8.533333s17.066667-2.133333 23.466667-8.533333L512 556.8 787.2 832c6.4 6.4 14.933333 8.533333 23.466667 8.533333s17.066667-2.133333 23.466666-8.533333c12.8-12.8 12.8-32 0-44.8L556.8 512z" fill="#666666" p-id="3579"></path></svg>
 				</li>`;
     }).join('');
+	localStorage.setItem('uploadUrls', JSON.stringify(uploadUrls));
 };
 
 // 更新下载列表
@@ -126,6 +142,6 @@ uploadWrapper.onclick = async (e) => {
 		const { fullname, index } = e.srcElement.dataset;
 		client.delete(fullname);
 		uploadUrls.splice(index, 1);
-		handleUpdateloadOl();
+		handleUploadOl();
 	}
 }  
